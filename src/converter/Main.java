31915c293aeaf0edae0f1f912bb281ba5fed82f9
iKinsure <59565620+iKinsure@users.noreply.github.com>
2022-03-14 20:15:37 +0100

    stage 2/4

--- src/converter/Main.java
+++ src/converter/Main.java
@@ -7,15 +7,41 @@ public class Main {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        while (true) {
+            System.out.println("Do you want to convert /from decimal or /to decimal? (To quit type /exit) ");
+            switch (scanner.nextLine()) {
+                case "/to":
+                    toDecimal();
+                    break;
+                case "/from":
+                    fromDecimal();
+                    break;
+                case "/exit":
+                    return;
+                default:
+            }
+        }
+    }
 
-        System.out.println("Enter number in decimal system: ");
-        int number = scanner.nextInt();
+    private static void fromDecimal() {
+        System.out.println("Enter a number in decimal system: ");
+        int number = Integer.parseInt(scanner.nextLine());
 
         System.out.println("Enter target base: ");
-        int base = scanner.nextInt();
+        int base = Integer.parseInt(scanner.nextLine());
 
         var result = Integer.toString(number, base);
         System.out.println("Conversion result: " + result);
+    }
 
+    private static void toDecimal() {
+        System.out.println("Enter source number: ");
+        var number = scanner.nextLine();
+
+        System.out.println("Enter source base: ");
+        int base = Integer.parseInt(scanner.nextLine());
+
+        var result = Integer.toString(Integer.parseInt(number, base), 10);
+        System.out.println("Conversion to decimal result: " + result);
     }
 }
