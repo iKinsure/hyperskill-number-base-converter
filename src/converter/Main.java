4433560d31167274ec16da2542306736f9b1dbe8
iKinsure <59565620+iKinsure@users.noreply.github.com>
2022-03-14 20:00:50 +0100

    stage 1/4

--- src/converter/Main.java
+++ src/converter/Main.java
@@ -1,8 +1,21 @@
 package converter;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // write your code here
+
+        System.out.println("Enter number in decimal system: ");
+        int number = scanner.nextInt();
+
+        System.out.println("Enter target base: ");
+        int base = scanner.nextInt();
+
+        var result = Integer.toString(number, base);
+        System.out.println("Conversion result: " + result);
+
     }
 }
